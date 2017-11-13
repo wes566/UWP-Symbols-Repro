ad5754180e8acdb53534970c1f1c24852b06abbf
Wesley Peter <wpeter@salesforce.com>
2017-11-13 10:44:28 -0800

    add build scirpt

---
+++ build.ps1
@@ -0,0 +1,74 @@
+function Get-ProgramFilesx86()
+{
+    $pf86 = ${env:ProgramFiles(x86)};
+    if (Test-Path $pf86)
+    {
+        return $pf86;
+    }
+
+    return $env:ProgramFiles;
+}
+
+function Get-MSBuild15()
+{
+    $pf86 = Get-ProgramFilesx86;
+    $msbuild = [IO.Path]::Combine($pf86, "Microsoft Visual Studio", "2017",
+        "Enterprise", "MSBuild", "15.0", "Bin", "msbuild.exe");
+
+    if (Test-Path $msbuild)
+    {
+        return $msbuild;
+    }
+
+    $msbuild = [IO.Path]::Combine($pf86, "Microsoft Visual Studio", "2017",
+        "Professional", "MSBuild", "15.0", "Bin", "msbuild.exe");
+
+    if (Test-Path $msbuild)
+    {
+        return $msbuild;
+    }
+
+    $msbuild = [IO.Path]::Combine($pf86, "Microsoft Visual Studio", "2017",
+        "Community", "MSBuild", "15.0", "Bin", "msbuild.exe");
+
+    if (Test-Path $msbuild)
+    {
+        return $msbuild;
+    }
+
+    throw "Visual Studio 2017 Enterprise or Professional or Community not installed";
+}
+
+function Get-MSBuild15Preview()
+{
+    $pf86 = Get-ProgramFilesx86;
+    $msbuild = [IO.Path]::Combine($pf86, "Microsoft Visual Studio", "2017",
+        "Enterprise", "MSBuild", "15.0", "Bin", "msbuild.exe");
+
+    if (Test-Path $msbuild)
+    {
+        return $msbuid;
+    }
+
+    $msbuild = [IO.Path]::Combine($pf86, "Microsoft Visual Studio", "2017",
+        "Professional", "MSBuild", "15.0", "Bin", "msbuild.exe");
+
+    if (Test-Path $msbuild)
+    {
+        return $msbuid;
+    }
+
+    $msbuild = [IO.Path]::Combine($pf86, "Microsoft Visual Studio", "2017",
+        "Community", "MSBuild", "15.0", "Bin", "msbuild.exe");
+
+    if (Test-Path $msbuild)
+    {
+        return $msbuid;
+    }
+
+    throw "Visual Studio 2017 Enterprise or Professional or Community not installed";
+}
+
+$msBuild = Get-MSBuild15;
+
+& $msBuild /t:clean,build UwpApp1.sln
